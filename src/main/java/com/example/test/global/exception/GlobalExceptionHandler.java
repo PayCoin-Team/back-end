b9bc654999ffc7b5069bfe8d65d9879d0f76f6ba
eastminnn <oh1334000@gmail.com>
2026-01-16 14:09:21 +0900

    feat : GlobalExceptionHandler 수정

--- src/main/java/com/example/test/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/test/global/exception/GlobalExceptionHandler.java
@@ -1,35 +1,45 @@
 package com.example.test.global.exception;
 
+import com.example.test.global.exception.error.ErrorCode;
+import com.example.test.global.exception.error.ErrorDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler{
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentExceptions(IllegalArgumentException ex) {
-
-        return ResponseEntity.status(400).body(ex.getMessage());
+    // 커스텀 예외처리
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<?> customExceptionHandler(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorDto errorDto = new ErrorDto(errorCode.getStatus(), errorCode.getMessage());
+        return new ResponseEntity<>(errorDto, HttpStatusCode.valueOf(errorCode.getStatus()));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(
-            MethodArgumentNotValidException ex
-    ) {
+    // 일반 예외처리
+    @ExceptionHandler
+    protected ResponseEntity<?> customServerException(Exception e){
+        log.error("INTERNAL_SERVER_ERROR", e);
+        ErrorDto errorDto = new ErrorDto(ErrorCode.INTERNAL_SERVER_ERROR.getStatus(), ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
-        return ResponseEntity.status(400).body(errorMessage);
+    // 메소드 인자 타당성 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for(FieldError error : e.getBindingResult().getFieldErrors())
+            errors.put(error.getField(), error.getDefaultMessage());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
-
-
-
-
-
-
-
-
-
-
