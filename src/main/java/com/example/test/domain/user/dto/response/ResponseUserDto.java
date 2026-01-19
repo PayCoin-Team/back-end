242ac27df6d02b4ddc15efe063914071cea39024
vkfro2293 <vkfro2293@naver.com>
2026-01-19 17:10:02 +0900

    feat: 회원 정보 조회에 잔액, public address 추가

--- src/main/java/com/example/test/domain/user/dto/response/ResponseUserDto.java
+++ src/main/java/com/example/test/domain/user/dto/response/ResponseUserDto.java
@@ -3,12 +3,16 @@ package com.example.test.domain.user.dto.response;
 import com.example.test.domain.user.entity.User;
 import lombok.Builder;
 
+import java.math.BigDecimal;
+
 @Builder
 public record ResponseUserDto(
         String username,
         String email,
         String firstName,
-        String lastName
+        String lastName,
+        BigDecimal balance,
+        String publicAddress
 ) {
     public static ResponseUserDto dtoToEntity(User user) {
         return ResponseUserDto.builder()
@@ -16,6 +20,8 @@ public record ResponseUserDto(
                 .email(user.getEmail())
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
+                .balance(user.getUserWallet().getBalance())
+                .publicAddress(user.getUserWallet().getPublicAddress())
                 .build();
     }
 }
