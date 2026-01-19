4446f3e2699d25335b4e0d41f7ab2992f803225b
Jang seheon <tpgjs3121@naver.com>
2026-01-19 13:50:35 +0900

    TronGrid Configuration 작성

--- src/main/java/com/example/test/domain/polling/config/TronGridConfig.java
+++ src/main/java/com/example/test/domain/polling/config/TronGridConfig.java
@@ -1,4 +1,34 @@
 package com.example.test.domain.polling.config;
 
+import io.netty.channel.ChannelOption;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@Configuration
 public class TronGridConfig {
+    @Bean
+    public WebClient tronGridWebClient(TronProperties properties){
+        TronProperties.Trongrid trongrid = properties.trongrid();
+
+        // 응답 타임아웃 설정
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofSeconds(10));
+
+        WebClient.Builder tron = WebClient.builder().baseUrl(trongrid.baseUrl())
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        if(trongrid.apiKey() != null && !trongrid.apiKey().isBlank())
+            tron.defaultHeader("TRON-PRO-API-KEY", trongrid.apiKey());
+
+        return tron.build();
+    }
 }
