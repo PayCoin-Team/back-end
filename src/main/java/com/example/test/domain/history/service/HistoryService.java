2efda0fa23effe5dbc8cb249355c4116dfd59f65
vkfro2293 <vkfro2293@naver.com>
2026-01-20 17:40:50 +0900

    feat: 내부 송금 요청 교착상태 방지 로직 구현

--- src/main/java/com/example/test/domain/history/service/HistoryService.java
+++ src/main/java/com/example/test/domain/history/service/HistoryService.java
@@ -6,43 +6,57 @@ import com.example.test.domain.history.dto.response.ResponseTransferDto;
 import com.example.test.domain.history.entity.History;
 import com.example.test.domain.history.enums.Type;
 import com.example.test.domain.history.repository.HistoryRepository;
-import com.example.test.domain.user.repository.UserRepository;
 import com.example.test.domain.userwallet.entity.UserWallet;
 import com.example.test.domain.userwallet.repository.UserWalletRepository;
 import com.example.test.global.exception.CustomException;
 import com.example.test.global.exception.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class HistoryService {
 
-    private final UserRepository userRepository;
     private final UserWalletRepository userWalletRepository;
     private final HistoryRepository historyRepository;
 
     // 내부 송금 요청
-    public ResponseTransferDto requestTransfer(Long userId, RequestTransferDto dto) {
+    public ResponseTransferDto requestTransfer(Long senderId, RequestTransferDto dto) {
 
-        // 보내는 사람 지갑
-        UserWallet senderWallet = userWalletRepository.findByUserIdWithLock(userId)
+        UserWallet receiverPreview = userWalletRepository.findByPublicAddress(dto.targetAddress())
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
-        // 받는 사람 지갑
-        UserWallet receiverWallet = userWalletRepository.findByPublicAddress(dto.targetAddress())
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
+
+        Long receiverId = receiverPreview.getId();
 
         // 자신에게 송금 차단
-        if(senderWallet.equals(receiverWallet)) {
+        if(senderId.equals(receiverId)) {
             throw new CustomException(ErrorCode.SELF_TRANSFER_INVALID);
         }
 
-        // 잔액 확인
-        if(senderWallet.getBalance().compareTo(dto.amount()) < 0 ) {
+        UserWallet senderWallet;
+        UserWallet receiverWallet;
+
+        // 교착 상태 방지 (회원 id 작은 순으로 lock 적용)
+        if(senderId < receiverId) {
+            senderWallet = userWalletRepository.findByUserIdWithLock(senderId)
+                    .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
+            receiverWallet = userWalletRepository.findByUserIdWithLock(receiverId)
+                    .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
+        } else {
+            receiverWallet = userWalletRepository.findByUserIdWithLock(receiverId)
+                    .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
+            senderWallet = userWalletRepository.findByUserIdWithLock(senderId)
+                    .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
+        }
+
+        // 잔액 확인 처리
+        if(senderWallet.getBalance().compareTo(dto.amount()) < 0) {
             throw new CustomException(ErrorCode.INSUFFICIENT_BALANCE);
         }
 
