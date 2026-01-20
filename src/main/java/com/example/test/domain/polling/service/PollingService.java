6fec4c3e3518aa87dd68678a27a04d441a704c3c
Jang seheon <tpgjs3121@naver.com>
2026-01-20 15:14:11 +0900

    Polling 로직 변경

--- src/main/java/com/example/test/domain/polling/service/PollingService.java
+++ src/main/java/com/example/test/domain/polling/service/PollingService.java
@@ -1,8 +1,8 @@
 package com.example.test.domain.polling.service;
 
 import com.example.test.domain.polling.config.TronProperties;
-import com.example.test.domain.polling.dto.TronEventData;
 import com.example.test.domain.polling.dto.TronGridResponse;
+import com.example.test.domain.polling.dto.TronTransfer;
 import com.example.test.domain.polling.model.Polling;
 import com.example.test.domain.polling.repository.PollingRepository;
 import com.example.test.global.exception.CustomException;
@@ -11,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
@@ -21,62 +24,88 @@ public class PollingService {
 
     private final TronProperties properties;
     private final PollingRepository pollingRepository;
+    private final WebClient tronGridClient;
 
     @Transactional
-    public void applyLatestBlockAndProcessEvents(TronGridResponse response) {
-
-        // 받은 데이터가 없는 경우
-        if (response == null || !response.success() || response.data() == null)
-            throw new CustomException(ErrorCode.TRON_API_ERROR);
-
-        List<TronEventData> events = response.data();
-
-        if (events.isEmpty()) {
-            log.info("새로운 트랜잭션이 없음");
-            return;
-        }
-
-        // 입출금 이벤트 처리
-        processEvents(events);
-
-        // lastBlock 업데이트
-        long latestBlock = events.stream().mapToLong(TronEventData::blockNumber)
-                .max().orElseThrow();
-
+    public void pollingUSDT(){
         Polling polling = pollingRepository.findById(1L)
                 .orElseThrow(() -> new CustomException(ErrorCode.ROW_NOT_FOUND));
 
+        long lastTimeStamp = polling.getLastTimestamp() + 1; // DB에 저장된 timestamp
+        long newTimeStamp = polling.getLastTimestamp();; // 갱신 후 새로 저장할 timestamp
+        String fingerprint = null;
 
-        if (latestBlock > polling.getLastBlock()) {
-            polling.setLastBlock(latestBlock);
-            log.info("폴링 완료: {} 번 블록까지 최신화되었습니다.", latestBlock);
+        while (true) {
+            final String fp = fingerprint;
+            TronGridResponse tron;
+
+            try {
+                tron = tronGridClient.get()
+                        .uri(uriBuilder -> {
+                            var b = uriBuilder
+                                    .path("/v1/accounts/{wallet}/transactions/trc20")
+                                    .queryParam("contract_address", properties.token().usdtContract())
+                                    .queryParam("only_confirmed", true)
+                                    .queryParam("order_by", "block_timestamp,asc")
+                                    .queryParam("min_timestamp", lastTimeStamp)
+                                    .queryParam("limit", 200);
+
+                            // api에서 받아온 데이터에 다음 페이지가 있으면 계속 가져오게 설정
+                            if (fp != null && !fp.isBlank()) {
+                                b.queryParam("fingerprint", fp);
+                            }
+                            return b.build(properties.wallet().serverAddress());
+                        })
+                        .retrieve()
+                        .bodyToMono(TronGridResponse.class)
+                        .block();
+            }catch (WebClientResponseException e){
+                throw new CustomException(ErrorCode.TRON_API_ERROR);
+            }
+
+            if (tron == null || tron.data() == null)
+                throw new CustomException(ErrorCode.TRON_API_ERROR);
+
+            // transfer 이벤트들 추출
+            List<TronTransfer> transfers = tron.data();
+
+            if (!transfers.isEmpty()) {
+                long firstTs = transfers.get(0).blockTimestamp();
+                long lastTs  = transfers.get(transfers.size() - 1).blockTimestamp();
+
+                log.info("이번 페이지 ts 범위(ms): first={}, last={}, count={}",
+                        firstTs, lastTs, transfers.size());
+            }
+
+            // 입출금 처리
+            handleTransfer(transfers);
+
+            if (!transfers.isEmpty()) {
+                long pageMax = transfers.stream()
+                        .mapToLong(TronTransfer::blockTimestamp)
+                        .max()
+                        .orElse(newTimeStamp);
+
+                newTimeStamp = Math.max(newTimeStamp, pageMax);
+            }
+
+            // 다음 페이지 조회를 위한 fingerprint 추출
+            String nextPage = null;
+            if (tron.meta() != null) nextPage = tron.meta().fingerprint();
+            if (nextPage == null || nextPage.isBlank()) break;
+            fingerprint = nextPage;
+        }
+
+        // timestamp 갱신
+        if (newTimeStamp > polling.getLastTimestamp()) {
+            polling.setLastTimestamp(newTimeStamp);
+            log.info("polling last_timestamp 업데이트");
         } else {
-            log.info("폴링 완료");
-        }
-
-    }
-
-    // 입출금 이벤트 처리
-    public void processEvents(List<TronEventData> events) {
-        String serverWallet = properties.wallet().serverAddress();
-
-        // polling으로 감지된 이벤트들 각각 처리
-        for (TronEventData event : events) {
-            String fromAddress = event.result().from();
-            String toAddress = event.result().to();
-            String txId = event.transactionId();
-
-            // 입금 감지: 받는 사람이 서버 지갑인 경우
-            if (serverWallet.equalsIgnoreCase(toAddress)) {
-                log.info("입금 감지");
-                // TODO: DB에서 txId 중복 확인 후, 해당 사용자의 내부 원장 잔액 증가 로직
-            }
-
-            // 출금 확정 감지: 보낸 사람이 서버 지갑인 경우
-            else if (serverWallet.equalsIgnoreCase(fromAddress)) {
-                log.info("출금 감지");
-                // TODO: DB에서 txId 중복 확인 후, 해당 사용자의 내부 원장 잔액 감소 로직
-            }
+            log.info("새로운 트랜잭션 없음");
         }
     }
+
+
+    // TODO: 입출금인지 확인 후 DB반영 처리 함수
+    private void handleTransfer(List<TronTransfer> transfers) {}
 }
--- src/main/java/com/example/test/domain/polling/service/TronScheduler.java
+++ src/main/java/com/example/test/domain/polling/service/TronScheduler.java
@@ -25,19 +25,7 @@ public class TronScheduler {
     @Scheduled(fixedDelayString = "${tron.polling.interval-ms}")
     @Transactional
     public void pollingUsdt(){
-
-        Polling polling = pollingRepository.findById(1L).orElseThrow(() -> new CustomException(ErrorCode.ROW_NOT_FOUND));
-
-        long startBlock = polling.getLastBlock();
-
-        tronGridClient.get().uri(uriBuilder -> uriBuilder.path("/v1/contracts/{address}/events") // polling api 경로
-                .queryParam("event_name", "Transfer")
-                .queryParam("order_by", "timestamp,asc")
-                .queryParam("min_block_number", startBlock + 1)
-                .build(properties.token().usdtContract())) // {address}에 컨트랙트 주소 넣기
-                .retrieve() // 응답 추출
-                .bodyToMono(TronGridResponse.class) // 응답Body를 자바 객체로
-                .subscribe(pollingService::applyLatestBlockAndProcessEvents);
+        pollingService.pollingUSDT();
     }
 
 }
