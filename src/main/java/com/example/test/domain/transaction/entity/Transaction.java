0e949132542c8bb111f4d568f63c141df90e8269
Jang seheon <tpgjs3121@naver.com>
2026-01-21 10:11:34 +0900

    Transaction 엔티티 추가

---
+++ src/main/java/com/example/test/domain/transaction/entity/Transaction.java
@@ -0,0 +1,58 @@
+package com.example.test.domain.transaction.entity;
+
+import com.example.test.global.time.BaseCreateEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import com.example.test.domain.externalWallet.entity.ExternalWallet;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "transaction")
+public class Transaction extends BaseCreateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "amount", nullable = false, precision = 18, scale = 6)
+    private BigDecimal amount;
+
+    @Size(max = 64)
+    @NotNull
+    @Column(name = "to_address", nullable = false, length = 64)
+    private String toAddress;
+
+    @Size(max = 64)
+    @Column(name = "from_address", length = 64)
+    private String fromAddress;
+
+    @Size(max = 100)
+    @Column(name = "txid", length = 100)
+    private String txid;
+
+    @Column(name = "log_index")
+    private Integer logIndex;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "type", nullable = false, length = 30)
+    private String type;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "status", nullable = false, length = 30)
+    private String status;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "externalwallet", nullable = false)
+    private ExternalWallet externalWallet;
+
+}
