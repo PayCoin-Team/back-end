05d0ec3e878df264d0a4cd4ca410148a03ee74d4
Jang seheon <tpgjs3121@naver.com>
2026-01-22 11:35:05 +0900

    지갑 잔고 조회 controller 생성

--- src/main/java/com/example/test/domain/polling/controller/TronController.java
+++ src/main/java/com/example/test/domain/polling/controller/TronController.java
@@ -1,4 +1,22 @@
 package com.example.test.domain.polling.controller;
 
+import com.example.test.domain.polling.dto.ServerBalnceResponse;
+import com.example.test.domain.polling.service.PollingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/server_wallet")
+@RequiredArgsConstructor
 public class TronController {
+
+    private final PollingService pollingService;
+
+    @GetMapping("/balance")
+    public ResponseEntity<ServerBalnceResponse> getVaultBalance() {
+        return ResponseEntity.ok(pollingService.getUsdtBalance());
+    }
 }
