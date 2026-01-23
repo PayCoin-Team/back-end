d1394c91e12fda8760f8bf234c762905fd70f360
Jang seheon <tpgjs3121@naver.com>
2026-01-23 11:29:32 +0900

    수수료 계산 서비스 구현

--- src/main/java/com/example/test/domain/admin/service/AdminService.java
+++ src/main/java/com/example/test/domain/admin/service/AdminService.java
@@ -1,11 +1,28 @@
 package com.example.test.domain.admin.service;
 
+import com.example.test.domain.admin.dto.FeeSummaryResponse;
+import com.example.test.domain.transaction.enums.Status;
 import com.example.test.domain.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
     private final TransactionRepository transactionRepository;
+
+    public FeeSummaryResponse getFees(){
+
+        //오늘 날짜의 자정
+        LocalDateTime today = LocalDate.now().atStartOfDay();
+
+        BigDecimal totalFees = transactionRepository.sumFees(Status.COMPLETED);
+        BigDecimal yesterdayFees = transactionRepository.sumFeesBeforeDate(today);
+
+        return new FeeSummaryResponse(totalFees, yesterdayFees);
+    }
 }
