ae367657b584abcff612a0fad2f0773a619a457f
Jang seheon <tpgjs3121@naver.com>
2026-01-26 13:12:51 +0900

    dto 및 서비스 리턴 값 추가

--- src/main/java/com/example/test/domain/polling/dto/ServerBalnceResponse.java
+++ src/main/java/com/example/test/domain/polling/dto/ServerBalnceResponse.java
@@ -3,5 +3,6 @@ package com.example.test.domain.polling.dto;
 import java.math.BigDecimal;
 
 public record ServerBalnceResponse(BigDecimal usdtBalance,
-                                   BigDecimal trxBalance) {
+                                   BigDecimal trxBalance,
+                                   String serverAddress) {
 }
