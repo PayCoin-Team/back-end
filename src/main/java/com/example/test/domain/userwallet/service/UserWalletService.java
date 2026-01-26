5db3b913a8bf911e09809e27a4b7dc074f85a40e
eastminnn <oh1334000@gmail.com>
2026-01-26 11:00:44 +0900

    feat : 유저 외부 지갑 조회 로직 추가

--- src/main/java/com/example/test/domain/userwallet/service/UserWalletService.java
+++ src/main/java/com/example/test/domain/userwallet/service/UserWalletService.java
@@ -1,5 +1,7 @@
 package com.example.test.domain.userwallet.service;
 
+import com.example.test.domain.externalWallet.entity.ExternalWallet;
+import com.example.test.domain.externalWallet.repository.ExternalWalletRepository;
 import com.example.test.domain.userwallet.dto.response.ResponseUserWalletDto;
 import com.example.test.domain.user.entity.User;
 import com.example.test.domain.userwallet.entity.UserWallet;
@@ -10,7 +12,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -18,14 +22,23 @@ import java.util.UUID;
 public class UserWalletService {
 
     private final UserWalletRepository userWalletRepository;
+    private final ExternalWalletRepository externalWalletRepository;
 
     // 사용자 내부 지갑 조회
     public ResponseUserWalletDto findUserWallet(Long userId) {
 
+        // 1. 내부 지갑 조회
         UserWallet userWallet = userWalletRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_WALLET_NOT_FOUND));
 
-        return ResponseUserWalletDto.dtoToEntity(userWallet);
+        // 2. 해당 유저의 외부 지갑 리스트 조회 및 주소 문자열만 추출
+        List<String> externalAddresses = externalWalletRepository.findAllByUserId(userId)
+                .stream()
+                .map(ExternalWallet::getAddress)
+                .collect(Collectors.toList());
+
+        // 3. 통합된 DTO 반환
+        return ResponseUserWalletDto.from(userWallet, externalAddresses);
     }
 
     // 내부 지갑 생성
