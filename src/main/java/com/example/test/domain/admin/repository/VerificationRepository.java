bcdb49472582d25f38ac700ba091fd860ab02b36
Jang seheon <tpgjs3121@naver.com>
2026-01-27 10:45:13 +0900

    jpa 수정

--- src/main/java/com/example/test/domain/admin/repository/VerificationRepository.java
+++ src/main/java/com/example/test/domain/admin/repository/VerificationRepository.java
@@ -3,6 +3,8 @@ package com.example.test.domain.admin.repository;
 import com.example.test.domain.admin.entity.Verification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VerificationRepository extends JpaRepository<Verification, Long> {
-    Verification findFirstByIdOrderByIdDesc();
+    Verification findFirstByOrderByIdDesc();
 }
