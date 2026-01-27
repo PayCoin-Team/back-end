81018b18f808322b381094bb8f8106c9eeedbbaa
Jang seheon <tpgjs3121@naver.com>
2026-01-27 10:21:31 +0900

    dto 수정

--- src/main/java/com/example/test/domain/admin/dto/FeeSummaryResponse.java
+++ src/main/java/com/example/test/domain/admin/dto/FeeSummaryResponse.java
@@ -6,5 +6,7 @@ import java.math.BigDecimal;
 
 
 public record FeeSummaryResponse(BigDecimal totalFees,
-                                 BigDecimal yesterdayFees) {
+                                 BigDecimal yesterdayFees,
+                                 BigDecimal serverBalance,
+                                 BigDecimal userBalance) {
 }
